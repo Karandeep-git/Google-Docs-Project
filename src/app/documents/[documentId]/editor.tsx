34c7972d8a07d5d957c90e89dbde5bc20e0c8577
Karandeep <softwaredevkaran@gmail.com>
2026-02-09 14:21:13 +0530

    Create and list documents

---
+++ src/app/documents/[documentId]/editor.tsx
@@ -0,0 +1,115 @@
+"use client";
+
+import { TaskList, TaskItem } from "@tiptap/extension-list";
+import { useEditor, EditorContent } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+import { TableKit } from "@tiptap/extension-table";
+import { Color } from "@tiptap/extension-color";
+import { Highlight } from "@tiptap/extension-highlight";
+import { Image } from "@tiptap/extension-image";
+import { ImageResize } from "tiptap-extension-resize-image";
+import { Underline } from '@tiptap/extension-underline'
+import { FontFamily, TextStyle } from "@tiptap/extension-text-style";
+import { Link } from "@tiptap/extension-link";
+import { TextAlign } from "@tiptap/extension-text-align";
+import { FontSizeExtension } from "@/extensions/font-size";
+import { LineHeightExtension } from "@/extensions/line-height";
+
+
+import { useEditorStore } from "@/store/use-editor-store";
+
+export const Editor = () => {
+
+  const { setEditor } = useEditorStore();
+
+  const editor = useEditor({
+    onCreate({ editor }) {
+      setEditor(editor)
+    },
+    onDestroy() {
+      setEditor(null);
+    },
+    onUpdate({ editor }) {
+      setEditor(editor)
+    },
+    onSelectionUpdate({ editor }) {
+      setEditor(editor)
+    },
+    onTransaction({ editor }) {
+      setEditor(editor)
+    },
+    onFocus({ editor }) {
+      setEditor(editor)
+    },
+    onBlur({ editor }) {
+      setEditor(editor)
+    },
+    onContentError({ editor }) {
+      setEditor(editor)
+    },
+    editorProps: {
+      attributes: {
+        style: "padding-left: 56px; padding-right: 56px;",
+        class:
+          "focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text",
+      },
+    },
+
+    extensions: [
+      StarterKit,
+      TaskList,
+      TaskItem.configure({
+        nested: true,
+      }),
+      TableKit,
+      Image,
+      ImageResize,
+      Underline,
+      FontFamily,
+      TextStyle,
+      Color,
+      Highlight.configure({
+        multicolor: true,
+      }),
+      Link.configure({
+        openOnClick: false,
+        autolink: true,
+        defaultProtocol: "https",
+      }),
+      TextAlign.configure({
+        types: ["heading", "paragraph"]
+      }),
+      FontSizeExtension,
+      LineHeightExtension.configure({
+        types: ["heading", "paragraph"],
+        defaultLineHeight: "normal"
+      })
+    ],
+
+    content: `
+        <table>
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <th colspan="3">Description</th>
+            </tr>
+            <tr>
+              <td>Cyndi Lauper</td>
+              <td>Singer</td>
+              <td>Songwriter</td>
+              <td>Actress</td>
+            </tr>
+          </tbody>
+        </table>
+      `,
+    immediatelyRender: false,
+  });
+  
+  return (
+    <div className="size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visible">
+      <div className="min-w-max flex justify-center w-204 py-4 print:py-0 mx-auto print:w-full print:min-w-0">
+        <EditorContent editor={editor} />
+      </div>
+    </div>
+  );
+};
---
+++ src/app/documents/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      Document Page
+    </div>
+  )
+}
+
+export default page
